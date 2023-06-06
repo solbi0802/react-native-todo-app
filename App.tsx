259579c42740eb888c6aa2f7f4621f00c15c6455
solbi0802 <bibi@lili-style.io>
2023-06-06 23:05:48 +0900

    header, todo input 추가

--- App.tsx
+++ App.tsx
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity,TextInput } from 'react-native';
+import { theme } from './color';
+import { SetStateAction, useState } from 'react';
 
 export default function App() {
+  const [working, setWorking] = useState(true)
+  const [text, setText] = useState("")
+  const onPressLife = () => setWorking(false)
+  const onPressWork = () => setWorking(true) 
+  const onChangeText = (payload: SetStateAction<string>) => setText(payload)
+
   return (
     <View style={styles.container}>
-      <Text>Hello!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity onPress={onPressWork}>
+        <Text style={{...styles.btnText, color: working ? "white": theme.grey}}>Work</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={onPressLife}>
+        <Text style={{...styles.btnText, color: working ? theme.grey : "white"}}>Life</Text>
+        </TouchableOpacity>
+      </View>
+        <TextInput onChangeText={onChangeText} value={text} returnKeyType="send" placeholder={"Add a Todo"} style={styles.input}/>
     </View>
   );
 }
@@ -13,8 +29,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor:  theme.background,
+    paddingHorizontal: 20
   },
-});
+  header: {
+    justifyContent: "space-between",
+    flexDirection: 'row',
+    marginTop: 100,
+  },
+  btnText: {
+    fontSize: 35,
+    fontWeight: "600"
+  },
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 15, 
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginTop: 20,
+    fontSize: 16
+  }
+})
---
+++ color.ts
@@ -0,0 +1,4 @@
+export const theme = {
+    background: "black",
+    grey: "#3A3D40"
+}
